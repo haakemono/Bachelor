d7f9e25c449dd9fe9b029c0704dcb8fece8236cc
Parsa Moniri <parsa.moniri@hotmail.com>
2025-01-14 14:04:02 +0100

    jkkjj

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ HEIGHT = 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Apples and Oranges")
 
+BG= pygame.transform.scale(pygame.image.load("img/background.jpg"), (WIDTH, HEIGHT))
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
 def main():
     run = True
     while run:
@@ -14,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw()
 
     pygame.quit()
 
