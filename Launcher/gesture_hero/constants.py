13f4411d6d5c13340248a1751491ce43edc17e45
Parsa Moniri <116574175+moneymoniri@users.noreply.github.com>
2025-05-11 21:11:34 +0200

    Update constants.py

--- Launcher/gesture_hero/constants.py
+++ Launcher/gesture_hero/constants.py
@@ -4,7 +4,7 @@ import pygame
 # dimensions
 WIDTH, HEIGHT = 1200, 800
 
-# colors
+#colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
@@ -13,22 +13,18 @@ BLUE = (0, 100, 255)
 GRAY = (100, 100, 100)
 PURPLE = (128, 0, 128)
 
-# notes
+#notes (WARNING: changing these can break existing songs)
 NOTE_SPEED = 6
 TOTAL_NOTES = 32 
 
-# keybinds (temp)
+#keybinds (remember to make corresponding changes in the beatmaps as well)
 KEYS = {'A': pygame.K_a, 'S': pygame.K_s, 'D': pygame.K_d, 'F': pygame.K_f}
 KEY_LABELS = ["A", "S", "D", "F"]
 
 # hit zone
 HIT_ZONE_X = 300  # where notes need to be hit
-HIT_TOLERANCE = 50  # how close a note needs to be to count as a hit (only counts BEFORE THE NOTE PASSES THE HIT ZONE)
-LATE_HIT_GRACE = 30 #tolerance for score increment despite hit after note passes hit zone
-
+HIT_TOLERANCE = 100  # how close a note needs to be to count as a hit, counts both before and after a note has passed
 # font
 pygame.font.init()
 FONT = pygame.font.Font(None, 50)
 
-#music file
-MUSIC_FILE = os.path.join(os.path.dirname(__file__), "music", "Level 1.mp3")
